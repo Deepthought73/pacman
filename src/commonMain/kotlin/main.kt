0ffd2353717be69a02f0f3d74168ca2125e40bad
kilian <ge95bit@mytum.de>
2022-03-07 20:38:53 +0100

    Game Map render

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,19 +1,15 @@
-import com.soywiz.klock.seconds
 import com.soywiz.korge.Korge
-import com.soywiz.korge.tween.get
-import com.soywiz.korge.tween.tween
-import com.soywiz.korge.view.*
+import com.soywiz.korge.view.image
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
-import com.soywiz.korma.geom.degrees
-import com.soywiz.korma.interpolation.Easing
 import model.GameBoard
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
-    val emptyGameBoardSprite = resourcesVfs["gameboard.png"].readBitmap()
-    val gameBoard = GameBoard(emptyGameBoardSprite)
+suspend fun main() = Korge(width = 224, height = 248, bgcolor = Colors["#000000"]) {
+    val emptyGameBoardBitmap = resourcesVfs["gameboard.png"].readBitmap()
+    val gameBoard = GameBoard(emptyGameBoardBitmap)
 
+    image(emptyGameBoardBitmap)
 
     /*
     var minDegrees = (-16).degrees
